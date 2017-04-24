f7d3630e0f30849c678be8c0fb56bc49cfc77f63
max <max@maxdeimac.local>
2017-04-24 11:43:10 +0800

    Initial Commit

---
+++ 转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by max on 2017/4/24.
+//  Copyright © 2017年 max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转场动画/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  转场动画
+//
+//  Created by max on 2017/4/24.
+//  Copyright © 2017年 max. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
